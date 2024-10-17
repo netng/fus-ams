c36603d149dea206e48b5c01a1ad850fa313e3f7
nandang <net.nandang@gmail.com>
2024-10-17 10:55:33 +0700

    finishing import feature asset type

--- app/models/asset_type.rb
+++ app/models/asset_type.rb
@@ -5,9 +5,10 @@ class AssetType < ApplicationRecord
   has_many :asset_item_types, dependent: :restrict_with_error
   has_many :asset_models, dependent: :restrict_with_error
 
-  downcase_fields :name
+  # downcase_fields :name
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
+  validates :id_asset_type, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 100 }
+  validates :name, presence: true, length: { maximum: 100 }
   validates :description, length: { maximum: 500 }
 
   def self.ransackable_associations(auth_object = nil)
@@ -15,6 +16,6 @@ class AssetType < ApplicationRecord
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "created_by", "description", "id", "ip_address", "name", "request_id", "updated_at", "user_agent"]
+    ["created_at", "created_by", "description", "id_asset_type", "id", "ip_address", "name", "request_id", "updated_at", "user_agent"]
   end
 end
