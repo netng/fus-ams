5d56b7cac64ee325644cd1117a8d5b0c70eda58a
nandang <net.nandang@gmail.com>
2024-12-05 16:10:40 +0700

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def human_readable_size(file_path)
+    if File.exist?(file_path)
+      file_size_in_bytes = File.size(file_path)
+      (file_size_in_bytes.to_f / 1_048_576).round(2)
+    else
+      0
+    end
+  end
 end
