529c5581a5d3eab0bb8458a03847c0f2e3014e58
nandang <net.nandang@gmail.com>
2025-01-22 16:25:12 +0700

    update

--- app/controllers/admin/files_controller.rb
+++ app/controllers/admin/files_controller.rb
@@ -1,41 +1,39 @@
 module Admin
   class FilesController < ApplicationAdminController
-    include Rails.application.routes.url_helpers
-
     def show
       skip_authorization
 
       blob = ActiveStorage::Blob.find_signed(params[:id])
 
       if blob
-        # Dapatkan URL sementara dari ActiveStorage
-        service_url = blob.url
-
-        # Gunakan streaming file dengan `send_file`
         headers['Content-Type'] = blob.content_type
-        headers['Content-Disposition'] = 'inline; filename="' + blob.filename.to_s + '"'
+        headers['Content-Disposition'] = "inline; filename=\"#{blob.filename}\""
         headers['Cache-Control'] = 'max-age=0, private, must-revalidate'
 
-        # Proxy file dari URL MinIO ke pengguna
-        stream_file(service_url)
+        # Jangan tetapkan Content-Length untuk menghindari mismatch
+        headers.delete('Content-Length')
+
+        stream_file(blob.url)
       else
         render plain: "File not found", status: :not_found
       end
     end
 
     private
-      def stream_file(url)
-        require 'open-uri'
 
-        # Buka URL sebagai stream
-        URI.open(url) do |stream|
-          while (chunk = stream.read(16.kilobytes))
-            response.stream.write(chunk)
-          end
+    def stream_file(url)
+      require 'open-uri'
+
+      URI.open(url) do |stream|
+        while (chunk = stream.read(16.kilobytes))
+          response.stream.write(chunk)
         end
+      rescue IOError
+        logger.error "Streaming interrupted"
       ensure
         response.stream.close
       end
-
+    end
   end
 end
+
